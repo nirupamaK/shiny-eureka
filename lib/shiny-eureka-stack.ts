040e7fde8132a213a86235aa9243603285a32ffb
Nirupama Karandikar <nirupama.karandikar@gmail.com>
2025-09-15 22:12:11 +0530

    Add few more bootstrap files

---
+++ lib/shiny-eureka-stack.ts
@@ -0,0 +1,59 @@
+import * as cdk from 'aws-cdk-lib';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as cpactions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+// import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class ShinyEurekaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+
+    const pipeline = new codepipeline.Pipeline(this, 'Entrix-App-Deploy-Pipeline', {
+      pipelineName: 'Entrix-App-Deploy-Pipeline',
+    });
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new cpactions.GitHubSourceAction({
+          actionName: 'GitHub_Source',
+          owner: 'nirupamak',
+          repo: 'laughing-waffle',
+          branch: 'main',
+          oauthToken: cdk.SecretValue.secretsManager('github-token-nk'),
+          output: sourceOutput,
+          trigger: cpactions.GitHubTrigger.NONE,
+        }),
+      ],
+    });
+    pipeline.addStage({
+      stageName: 'BuildAndDeploy',
+      actions: [
+        new cpactions.CodeBuildAction({
+          actionName: 'Build',
+          project: new codebuild.PipelineProject(this, 'BuildProject', {
+            buildSpec: codebuild.BuildSpec.fromObject({
+              version: '0.2',
+              phases: {
+                install: {
+                  commands: ['npm install'],
+                },
+                build: {
+                  commands: ['cdk deploy --require-approval never'],
+                },
+              },
+            }),
+          }),
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
+      ],
+    });
+
+
+  }
+}
---
+++ test/shiny-eureka.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShinyEureka from '../lib/shiny-eureka-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shiny-eureka-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShinyEureka.ShinyEurekaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
